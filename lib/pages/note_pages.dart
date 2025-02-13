b5767578747454ba8e5dcd55131f389cbf72d2a7
Etpaxtysus <itsrhenaldy@gmail.com>
2025-02-13 18:21:06 +0700

    feat: add notes page

---
+++ lib/pages/note_pages.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NotePages extends StatefulWidget {
+  const NotePages({super.key});
+
+  @override
+  State<NotePages> createState() => _NotePagesState();
+}
+
+class _NotePagesState extends State<NotePages> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
