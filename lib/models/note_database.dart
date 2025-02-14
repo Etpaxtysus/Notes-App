87dc83dc18fc7264192a80d51945fd4db6d7acc2
Etpaxtysus <itsrhenaldy@gmail.com>
2025-02-14 17:05:04 +0700

    update: add CRUD for notes

--- lib/models/note_database.dart
+++ lib/models/note_database.dart
@@ -9,11 +9,39 @@ class NoteDatabase {
     final dir = await getApplicationDocumentsDirectory();
     isar = await Isar.open([NoteSchema], directory: dir.path);
   }
+
+  // List of Notes :
+  final List<Note> currentNotes = [];
+
   // Create
+  Future<void> createNote(String textFromUser) async {
+    final newNote = Note()..text = textFromUser;
+
+    await isar.writeTxn(() => isar.notes.put(newNote));
+
+    fetchNotes();
+  }
 
   // Read
+  Future<void> fetchNotes() async {
+    List<Note> fetchedNotes = await isar.notes.where().findAll();
+    currentNotes.clear();
+    currentNotes.addAll(fetchedNotes);
+  }
 
   // Update
+  Future<void> updateNote(int id, String newText) async {
+    final existingNote = await isar.notes.get(id);
+    if (existingNote != null) {
+      existingNote.text = newText;
+      await isar.writeTxn(() => isar.notes.put(existingNote));
+      fetchNotes();
+    }
+  }
 
   // Delete
+  Future<void> deleteNote(int id) async {
+    await isar.writeTxn(() => isar.notes.delete(id));
+    fetchNotes();
+  }
 }
