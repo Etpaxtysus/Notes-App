70555617c4c5e2bfddf758514b186af617ee1e89
Etpaxtysus <itsrhenaldy@gmail.com>
2025-02-14 16:43:49 +0700

    update: init database

--- lib/models/note_database.dart
+++ lib/models/note_database.dart
@@ -1,13 +1,19 @@
+import 'package:isar/isar.dart';
+import 'package:notesapp/models/note_model.dart';
+import 'package:path_provider/path_provider.dart';
+
 class NoteDatabase {
+  static late Isar isar;
+  // Initialize Database
+  static Future<void> initialize() async {
+    final dir = await getApplicationDocumentsDirectory();
+    isar = await Isar.open([NoteSchema], directory: dir.path);
+  }
+  // Create
 
- // Initialize Databse
+  // Read
 
- // Create
+  // Update
 
- // Read
-
- // Update
-
- // Delete
-
+  // Delete
 }
