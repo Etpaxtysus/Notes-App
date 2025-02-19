2af8f3270246405a28f1b8dfd6469bd29117cbaf
Etpaxtysus <itsrhenaldy@gmail.com>
2025-02-19 20:20:16 +0700

    update: add Theme Provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:notesapp/models/note_database.dart';
 import 'package:notesapp/pages/note_page.dart';
+import 'package:notesapp/theme/theme_provider.dart';
 import 'package:provider/provider.dart';
 
 void main() async {
   // Init Note Isar Database
   WidgetsFlutterBinding.ensureInitialized();
   await NoteDatabase.initialize();
-  runApp(ChangeNotifierProvider(
-    create: (context) => NoteDatabase(),
+  runApp(MultiProvider(
+    providers: [
+      // Note Provider
+      ChangeNotifierProvider(create: (context) => NoteDatabase()),
+      // Theme Provider
+      ChangeNotifierProvider(create: (context) => ThemeProvider())
+    ],
     child: const MyApp(),
   ));
 }
@@ -20,7 +26,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: NotePages(),
+      home: const NotePages(),
+      theme: Provider.of<ThemeProvider>(context).themeData,
     );
   }
 }
